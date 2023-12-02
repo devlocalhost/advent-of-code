baeb6abc56ca2821e582fce888bc68e6556bab0a
devlocalhost <lazarecobani@gmail.com>
2023-12-02 17:00:27 +0200

    improve code

--- 2023/day2/part1/solution.py
+++ 2023/day2/part1/solution.py
@@ -2,29 +2,18 @@ limits = {"red": 12, "green": 13, "blue": 14}
 valid_games = []
 
 with open("input.txt", encoding="utf-8") as file:
-    lines = file.readlines()
-
-    for line in lines:
+    for line in file.readlines():
         game_id = line.split(":")[0].split(" ")[1]
         cubes = [i.strip() for i in line.split(":")[1].strip().split(";")]
 
-        cubes_len = 0
         invalid_cubes = 0
 
         for cubes_list in cubes:
-            cubes_len += 1
-
             for i in cubes_list.split(", "):
                 cube_color = i.split(" ")[1]
                 cube_count = int(i.split(" ")[0])
 
-                cube_color_limit = limits[cube_color]
-
-                if cube_count <= limits[cube_color]:
-                    print(f" --- GAME {game_id}                 VALID --- ")
-
-                else:
-                    print(f" --- GAME {game_id}             NOT VALID --- ")
+                if cube_count > limits[cube_color]:
                     invalid_cubes += 1
 
         if invalid_cubes == 0:
